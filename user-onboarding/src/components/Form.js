4eeefd77db603b0f4eaf67f12d7828a84802a715
Brandon Maroni <brandon-maroni@lambdastudents.com>
2020-09-08 18:24:37 -0600

    created formSchema, Form function, formState

--- user-onboarding/src/components/Form.js
+++ user-onboarding/src/components/Form.js
@@ -1,10 +1,44 @@
 import React, { useState } from 'react';
+import * as yup from 'yup';
+import axios from 'axios';
+
+const formSchema = yup.object().shape({
+    name: '',
+    email: '',
+    password: '',
+    terms: false
+})
 
 const Form = () => {
 
-    return (
+    const [formState, setFormState] = useState({
+        name: '',
+        email: '',
+        password: '',
+        terms: false
+    });
 
-    );
+    return (
+        <form>
+            <label>
+                Name
+                <input type="text" name="name" id="name" ></input>
+            </label>
+            <label>
+                Email
+                <input type="text" name="email" id="email" ></input>
+            </label>
+            <label>
+                Password
+                <input type="text" name="password" id="password" ></input>
+            </label>
+            <label>
+                I have read and agree to Terms & Conditions
+                <input type='checkbox' name='terms'></input>
+            </label>
+            <button>Submit</button>
+        </form>
+    )
 };
 
 export default Form
